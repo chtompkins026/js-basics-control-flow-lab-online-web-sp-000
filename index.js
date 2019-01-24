c53a8ce5f39e0464f0aeb2ee76a6135cfa78cc49
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-24 04:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(dist) {
+  let msg;
+  if (dist <= 400) {
+    msg = "This one is on me!";
+  } else if (dist < 2500) {
+    msg = "I will gladly take your thirty bucks.";
+  } else {
+    msg = "No can do.";
+  }
+  return msg;
+}
+
+function ternaryCheckCity(city) {
+  let msg = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return msg;
+}
+
+function switchOnCharmFromTip(tip) {
+ switch(tip) { 
+   case tip >= 
+}
